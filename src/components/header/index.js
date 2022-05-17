fddb307cb84c31d2809b524ee5d4dc26d1b4786d
Sujith Patil <emailsp019@gmail.com>
2022-05-17 14:23:25 +0530

    adding header icons

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -14,10 +14,22 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link className="nav-link" to="/models">Model</Link>
-                        <Link className="nav-link" to="/dataset">Dataset</Link>
-                        <Link className="nav-link" to="/how-to-use">How to use?</Link>
-                        <Link className="nav-link" to="/contact-us">Contact us</Link>
+                        <div className='nav-link-block'>
+                            <img src='./icons/cube.svg' alt='' />
+                            <Link className="nav-link" to="/models">Model</Link>
+                        </div>
+                        <div className="nav-link-block">
+                            <img src='./icons/database.svg' alt='' />
+                            <Link className="nav-link" to="/dataset">Dataset</Link>
+                        </div>
+                        <div className="nav-link-block">
+                            <img src='./icons/document.svg' alt='' />
+                            <Link className="nav-link" to="/how-to-use">How to use?</Link>
+                        </div>
+                        <div className="nav-link-block">
+                            <img src='./icons/email.svg' alt='' />
+                            <Link className="nav-link" to="/contact-us">Contact us</Link>
+                        </div> 
                     </Nav>
                 </Navbar.Collapse>
             </Container>
